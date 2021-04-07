cc5c4db828fc271246472ec1ec7aa8c1caca0c27
Carl Nygard <carl.nygard@thoughtworks.com>
2021-04-07 00:19:36 -0400

    add configuration for open policy agent
    - currently, just disable csrf

---
+++ open-policy-agent-config/src/main/java/io/twdps/starter/boot/config/OpenPolicyAgentConfig.java
@@ -0,0 +1,25 @@
+package io.twdps.starter.boot.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+
+@Configuration
+@ConditionalOnProperty(prefix = "starter.open-policy-agent-config", name = "enabled", havingValue = "true", matchIfMissing = true)
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableWebSecurity
+@Import(SecurityProblemSupport.class)
+public class OpenPolicyAgentConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf()
+        .disable();
+  }
+}
